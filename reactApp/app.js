1fc5bdc8791d87a6a29e4506c5c1185badca25b8
Jay <jawan94@gmail.com>
2017-10-06 16:06:52 -0700

    update

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+var dummyData = ['shower', 'sleep', 'eat', 'workout', 'study']
 
 
-ReactDOM.render(<p>This is cool</p>,
+class TodoList extends React.Component {
+constructor(props) {
+  super(props);
+}
+render() {
+  return (
+  <ul>
+   {dummyData.map((item) => <Todo task = {item}/>)}
+  </ul>
+    )
+  }
+}
+
+class Todo extends React.Component {
+constructor(props){
+  super(props)
+}
+render() {
+  return(
+    <div>
+    <li> <button className= "btn btn-default">x</button>
+    {this.props.task} </li>
+    </div>
+  )
+  }
+}
+
+class InputLine extends React.Component {
+constructor(props){
+  super(props)
+}
+render() {
+return (
+  <div>
+  <input type="text" value=""/>
+  <input type="submit" value="Add todo"/>
+  </div>
+    )
+  }
+}
+
+class TodoApp extends React.Component {
+constructor(props){
+  super(props)
+}
+  render() {
+    return(
+      <div>
+  <InputLine/>
+  <TodoList/>
+  </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<TodoApp />,
    document.getElementById('root'));
